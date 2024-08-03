36b575ba5124dc1ce60641250db07fc32590fe95
Eduardo José Zang <zangeduardo2016@gmail.com>
2024-08-03 19:10:47 -0300

    ajuste nas permissões

--- manage_account.php
+++ manage_account.php
@@ -39,7 +39,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <li><a href="about_us.php">Quem Somos</a></li>
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="manage_account.php">Minha Conta</a></li>
-                    <li><a href="manage_posts.php">Gerenciar Publicações</a></li>
+                    <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == 1): ?>
+                        <li><a href="manage_posts.php">Gerenciar Publicações</a></li>
+                    <?php endif; ?>
                     <li><a href="logout.php">Sair</a></li>
                 <?php else: ?>
                     <li><a href="login.php">Login</a></li>
