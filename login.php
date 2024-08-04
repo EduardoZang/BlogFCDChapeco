d8f3e7c75d2266885cab3b7b753ba2034958d6d4
Eduardo José Zang <zangeduardo2016@gmail.com>
2024-08-04 20:14:30 -0300

    atualização na tela de login

--- login.php
+++ login.php
@@ -2,12 +2,19 @@
 session_start();
 include 'db.php';
 
+$error = '';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = htmlspecialchars($_POST['username']);
+    $identifier = htmlspecialchars($_POST['identifier']);
     $password = htmlspecialchars($_POST['password']);
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-    $stmt->execute([$username]);
+    if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) {
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    } else {
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    }
+    
+    $stmt->execute([$identifier]);
     $user = $stmt->fetch();
     
     if ($user && password_verify($password, $user['password'])) {
@@ -19,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header('Location: index.php');
         exit;
     } else {
-        $error = "Nome de usuário ou senha incorretos.";
+        $error = "Nome de usuário, e-mail ou senha incorretos.";
     }
 }
 ?>
@@ -29,24 +36,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
     <?php include 'templates/header.php'; ?>
-    <h2>Login</h2>
-    <?php if (isset($error)): ?>
-        <p><?php echo $error; ?></p>
-    <?php endif; ?>
-    <form action="login.php" method="post">
-        <label for="username">Nome de Usuário:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="password">Senha:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <input type="submit" value="Login">
-    </form>
+
+    <div class="content-wrapper">
+        <div class="form-container">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <label for="identifier">Nome de Usuário ou E-mail:</label>
+                <input type="text" id="identifier" name="identifier" required>
+                <br>
+                <label for="password">Senha:</label>
+                <input type="password" id="password" name="password" required>
+                <br>
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+    </div>
+
+        <!-- Modal de Erro -->
+    <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="errorModalLabel">
+                        <i class="fas fa-exclamation-triangle"></i> Erro
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php echo $error; ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php include 'templates/footer.php'; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        <?php if ($error): ?>
+            $(document).ready(function() {
+                $('#errorModal').modal('show');
+            });
+        <?php endif; ?>
+    </script>
 </body>
 </html>
