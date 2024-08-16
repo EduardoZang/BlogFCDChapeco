9297dcc1695ebc62f8ade4a4e6138668bbefeb24
Eduardo José Zang <zangeduardo2016@gmail.com>
2024-08-15 22:09:52 -0300

    atualização de links

--- templates/header.php
+++ templates/header.php
@@ -48,7 +48,7 @@
                 </div>
             </nav>
             <div class="social-icons">
-                <a href="https://www.facebook.com/fcdchapeco/?locale=pt_BR" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.facebook.com/people/Fraternidade-Crist%C3%A3-Chapec%C3%B3/pfbid0FxsA1FFLxSthCEztgC9fdwRpYDfLBKhboEqDBEBd1Hnf1SPMU3jLT376CYzri1ETl/" target="_blank"><i class="fab fa-facebook-f"></i></a>
                 <a href="https://www.instagram.com/explore/locations/841625812615423/fraternidade-crista-de-doentes-e-deficientes-de-chapeco-scfc/" target="_blank"><i class="fab fa-instagram"></i></a>
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li class="nav-item">
