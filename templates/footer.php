5fe35ec42a979e4c89621a94219355feb07b1111
Eduardo José Zang <zangeduardo2016@gmail.com>
2024-08-17 22:08:36 -0300

    atualização "footer"

--- templates/footer.php
+++ templates/footer.php
@@ -3,15 +3,21 @@
 </head>
 <footer>
     <div class="footer-main">
-        <img src="assets/images/logoFCD.png" alt="Logo" class="footer-logo-main">
+        <img src="assets/images/logoFCD.jpeg" alt="Logo" class="footer-logo-main">
         <div class="footer-section">
-            <h2>Contatos</h2>
+            <h5 class="footer_subtitle">Contatos</h5>
+            <span class="footer_description"> Telefone: (49) 3331 34-81</span>
+            <br>
+            <span class="footer_description"> E-mail: fcdchapeco@gmail.com</span>
         </div>
         <div class="footer-section">
-            <h2>Links úteis</h2>
-        </div>
-        <div class="footer-section">
-            <h2>Endereço</h2>
+            <h5 class="footer_subtitle">Endereço</h5>
+            <span class="footer_description"> Rua São Leopoldo 461-D. Bairro Esplanada.</span>
+            <br>
+            <span class="footer_description"> CEP: 89.812-565 </span>
+            <br>
+            <span class="footer_description"> Chapecó - SC</span>
+            
         </div>
     </div>
     <div class="footer-secondary">
